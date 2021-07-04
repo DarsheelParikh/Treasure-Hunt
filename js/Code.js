19be213a26abfc28aec847052020b5a5acb3e4df
DarsheelParikh <76078295+DarsheelParikh@users.noreply.github.com>
2021-07-04 17:55:18 -0400

    Add files via upload

---
+++ js/Code.js
@@ -0,0 +1,27 @@
+const accessCode1 = "VARIABLE";
+const accessCode2 = "FUNCTION";
+const accessCode3 = "DATABASE";
+
+function clues() {
+    
+    fill("white")
+    textSize(15)
+    text("R E V B A I L A", 100,50)
+    fill("lightblue")
+    text("Hint: Always changing, not constant !!", 100,70)
+
+    fill("white")
+    textSize(15)
+    text("C U T N I F O N", 575,150)
+    fill("lightblue")
+    text("Performs a particular task !!", 575,170)
+
+    fill("white")
+    textSize(15)
+    text("A T A D A S B A", 150,270)
+    fill("lightblue")
+    text("Hint: Stores all information !!", 150,290)
+// add code for displaying rest of the names and the hints.
+    
+
+}
